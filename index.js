44e667365d07be784cfd62fa55c6b50496cdfa49
cibernox <miguel.camba@gmail.com>
2018-01-11 23:39:21 +0100

    Make it a noop in Ember > 3.0.0-beta.2

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 'use strict';
 
+let VersionChecker = require('ember-cli-version-checker');
+
 module.exports = {
-  name: 'ember-native-dom-event-dispatcher'
+  name: 'ember-native-dom-event-dispatcher',
+  init() {
+    this._super && this._super.init.apply(this, arguments);
+
+    let checker = new VersionChecker(this);
+    this._shouldDisable = checker.forEmber().gt('3.0.0-beta.2');
+  },
+
+  treeFor() {
+    if (this._shouldDisable) {
+      return null;
+    }
+
+    return this._super.treeFor.apply(this, arguments);
+  }
 };
